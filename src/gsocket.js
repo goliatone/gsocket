61feb293da156bef2e52f55b2cd79e4130b69b54
goliatone <burgosemi@gmail.com>
2014-07-15 10:12:51 -0400

    SRC: simplify AMD code

--- src/gsocket.js
+++ src/gsocket.js
@@ -7,35 +7,7 @@
  */
 /* jshint strict: false, plusplus: true */
 /*global define: false, require: false, module: false, exports: false */
-(function(root, name, deps, factory) {
-    "use strict";
-    // Node
-    if (typeof deps === 'function') {
-        factory = deps;
-        deps = [];
-    }
-
-    if (typeof exports === 'object') {
-        module.exports = factory.apply(root, deps.map(require));
-    } else if (typeof define === 'function' && 'amd' in define) {
-        //require js, here we assume the file is named as the lower
-        //case module name.
-        define(name.toLowerCase(), deps, factory);
-    } else {
-        // Browser
-        var d, i = 0,
-            global = root,
-            old = global[name],
-            mod;
-        while ((d = deps[i]) !== undefined) deps[i++] = root[d];
-        global[name] = mod = factory.apply(global, deps);
-        //Export no 'conflict module', aliases the module.
-        mod.noConflict = function() {
-            global[name] = old;
-            return mod;
-        };
-    }
-}(this, "GSocket", ["extend"], function(extend) {
+define('gsocket', ['extend'], function(extend) {
 
     /**
      * Extend method.
@@ -140,4 +112,4 @@
     };
 
     return GSocket;
-}));
+});

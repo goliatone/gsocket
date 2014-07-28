316ef5632a0b80796fd6278146a14583304237e8
goliatone <burgosemi@gmail.com>
2014-07-28 10:59:46 -0400

    Gruntfile: connect to different port to run example.

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
         },
         connect: {
             options: {
-                port: 9000,
+                port: 9696,
                 // Change this to '0.0.0.0' to access the server from outside.
                 hostname: 'localhost'
             },
